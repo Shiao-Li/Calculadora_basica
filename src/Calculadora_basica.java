ec405aa8c2497569832d78b68c9958a5b0548b33
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-01-23 22:18:49 -0500

    Interfaz y numeros

--- src/Calculadora_basica.java
+++ src/Calculadora_basica.java
@@ -1,2 +1,117 @@
-package PACKAGE_NAME;public class Calculadora_basica {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculadora_basica {
+
+
+    private JPanel Panel_calculadora;
+    private JButton SieteButton;
+    private JButton CuatroButton;
+    private JButton OchoButton;
+    private JButton NueveButton;
+    private JButton RestaButton;
+    private JButton CincoButton;
+    private JButton SeisButton;
+    private JButton MultiplicacionButton;
+    private JButton UnoButton;
+    private JButton DosButton;
+    private JButton TresButton;
+    private JButton DivisionButton;
+    private JButton ACButton;
+    private JButton DELButton;
+    private JButton SumaButton;
+    private JButton CeroButton;
+    private JButton PuntoButton17;
+    private JButton IgualButton;
+    private JTextArea OperacionesText;
+    private JTextArea RessultadoText;
+
+    public Calculadora_basica() {
+        CeroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+CeroButton.getText());
+            }
+        });
+        UnoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+UnoButton.getText());
+            }
+        });
+        DosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+DosButton.getText());
+            }
+        });
+        TresButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+TresButton.getText());
+            }
+        });
+
+        CuatroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+CuatroButton.getText());
+            }
+        });
+
+        CincoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+CincoButton.getText());
+            }
+        });
+
+        SeisButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+SeisButton.getText());
+            }
+        });
+
+
+        SieteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+SieteButton.getText());
+            }
+        });
+
+        OchoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+OchoButton.getText());
+            }
+        });
+
+        NueveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(OperacionesText.getText()+NueveButton.getText());
+            }
+        });
+
+        ACButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OperacionesText.setText(" ");
+                RessultadoText.setText(" ");
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame1 = new JFrame("Poli Calcu");
+        frame1.setContentPane(new Calculadora_basica().Panel_calculadora);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.pack();
+        frame1.setVisible(true);
+    }
+
 }
